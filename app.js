62e02759493be656a703793e964996f63d301947
lovelybre132-spec <lovelybre132@gmail.com>
2026-01-11 19:55:32 +0000

    add lead

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function saveData() {
+const SUPABASE_URL = 'https://zwtdeaqbeytnllldqbbl.supabase.co/rest/v1/LEAD';
+const SUPABASE_KEY =   'sb_publishable_hfN4Ncv5oQT7HS9IgwcA9g_LPa4pCw_';
+async function saveData() {
     event.preventDefault();
     // Function to save data
     let contactName = document.getElementById('contactName').value;
@@ -9,5 +11,14 @@ let user={
     email:contactEmail,
     message:contactMessage
 };
+let response = await fetch(SUPABASE_URL, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'apikey': SUPABASE_KEY,
+    },
+    body: JSON.stringify(user)
+});
 console.log(user);
+console.log(response);
 }
