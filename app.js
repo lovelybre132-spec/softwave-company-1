2c163b8ff2f45d0ccaf78ca15b7e7a73ad756429
lovelybre132-spec <lovelybre132@gmail.com>
2026-01-19 22:09:54 +0000

    render cards

--- app.js
+++ app.js
@@ -22,3 +22,32 @@ let response = await fetch(SUPABASE_URL, {
 console.log(user);
 console.log(response);
 }
+async function loadProducts() {
+    let response = await fetch('https://zwtdeaqbeytnllldqbbl.supabase.co/rest/v1/product', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3dGRlYXFiZXl0bmxsbGRxYmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NjYxMzAsImV4cCI6MjA4MTE0MjEzMH0.hoNlMu_eUfizpyiDcxRulr6fZhHV4yIFWmYC3K2cyrE',
+        }
+    });
+    let products = await response.json();
+    let productListDiv = document.getElementById('productList');
+    products.forEach(product => {
+        let productCard = document.createElement('div');
+        productCard.className = 'col-md-6 col-lg-3';
+        productCard.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img src="${product.image_url}" class="card-img-top" alt="${product.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p class="card-text"><strong>Price: $${product.price}</strong></p>
+                </div>
+            </div>
+        `;
+        productListDiv.appendChild(productCard);
+    });
+}
+window.onload = function() {
+    loadProducts();
+};
